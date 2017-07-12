f64014ee8d5cddb4f5272756ae040533ac23d96d
ChaoranLI <chaoran.li@groupe-esigelec.org>
2017-07-12 17:48:18 +0200

    LI2017-07-12

--- Kraze/MainVC.swift
+++ Kraze/MainVC.swift
@@ -8,15 +8,26 @@
 
 import UIKit
 import Firebase
+import FacebookLogin
+import FacebookCore
+import FBSDKLoginKit
+import FBSDKCoreKit
+import FirebaseAuth
 
-class MainVC: UIViewController, UITextFieldDelegate{
+class MainVC: UIViewController, UITextFieldDelegate, FBSDKLoginButtonDelegate{
 
+    @IBOutlet weak var FacebookLogin: FBSDKLoginButton!
     @IBOutlet weak var userLoginEmail: UITextField!
     @IBOutlet weak var userLoginPassword: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let facebookLogin = self.FacebookLogin
+        facebookLogin?.delegate = self
+        facebookLogin?.readPermissions = ["email", "public_profile"]
+        
         
         let userLoginEmail = self.userLoginEmail
         let userLoginPassword = self.userLoginPassword
@@ -105,7 +116,56 @@ class MainVC: UIViewController, UITextFieldDelegate{
         self.present(myAlert, animated:true, completion:nil)
         
     }
-
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did log out of facebook")
+    }
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil{
+            print(error)
+        }
+        showInformation()
+    }
+    func showInformation(){
+        let accessToken = FBSDKAccessToken.current()
+        let credentials = FacebookAuthProvider.credential(withAccessToken: (accessToken?.tokenString)!)
+        Auth.auth().signIn(with: credentials) { (user, error) in
+            if error != nil{
+                print("Something went wrong with our FB user:", error)
+                return
+            }
+            print("Successfully logged in with our FB user:", user)
+            guard let uid = user?.uid else{
+                return
+            }
+            let ref: DatabaseReference!
+            ref = Database.database().reference(fromURL: "https://kraze-935a0.firebaseio.com/")
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"email, first_name, last_name, id, picture.type(large)"]).start(completionHandler:  { (connection, result, error) -> Void in
+                if error != nil{
+                    print("Failed to start graph request:", error)
+                    return
+                }
+                let userdict = result as! NSDictionary
+                let firstName = userdict["first_name"] as! String
+                let lastName = userdict["last_name"] as! String
+                let email = userdict["email"] as! String
+                let id = userdict["id"] as! String
+                print(firstName)
+                print(lastName)
+                print(email)
+                print(userdict)
+                let usersReference = ref.child("FacebookUsers").child(uid)
+                let values = ["Firstname": firstName, "Lastname": lastName, "email": email, "id" :id]
+                usersReference.updateChildValues(values, withCompletionBlock: { (err, ref) in
+                    if err != nil{
+                        print(err)
+                        return
+                    }
+                    print("Saved user successfully into Firebase db")
+                })
+                
+            })
+        }
+    }
     /*
     // MARK: - Navigation
 
